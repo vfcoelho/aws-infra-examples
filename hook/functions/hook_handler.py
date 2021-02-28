d24aff26960aa393adb76a1a2e510c6856455bc7
vfcoelho <victor.coelho@ambev.com.br>
2021-02-28 01:13:24 -0300

    adds exception example

--- hook/functions/hook_handler.py
+++ hook/functions/hook_handler.py
@@ -3,6 +3,15 @@ logging.basicConfig()
 log = logging.getLogger()
 log.setLevel(logging.INFO)
 
+import json
+
 def receive_data(event,context):
     log.info('Event Received!')
-    log.info(event['body'])
+
+    message = event['Records'][0]
+    call = json.loads(message.get('body','{}'))
+
+    log.info(call)
+
+    if call.get('body',{}).get('raise'):
+        raise Exception('Error raised manually')

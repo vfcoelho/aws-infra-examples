5e17ba73456435a0a2c6fffaea0f20f8da4cf848
vfcoelho <victor.coelho@ambev.com.br>
2021-02-28 01:53:35 -0300

    improves manual raise logic

--- hook/functions/hook_handler.py
+++ hook/functions/hook_handler.py
@@ -7,11 +7,17 @@ import json
 
 def receive_data(event,context):
     log.info('Event Received!')
+    log.info(event)
 
     message = event['Records'][0]
     call = json.loads(message.get('body','{}'))
 
     log.info(call)
 
-    if call.get('body',{}).get('raise'):
+    type_node_raise = {
+        'Hook': lambda call: call.get('body',{}).get('raise'),
+        'Notification': lambda call: json.loads(call.get('Message','{}')).get('raise')
+    }
+
+    if type_node_raise[call.get('Type')](call):
         raise Exception('Error raised manually')
